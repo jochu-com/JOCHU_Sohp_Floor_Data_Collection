69356642d9d04707bc7435e8ae8a921cdef70897
jochu-com <jochu-com@users.noreply.github.com>
2026-02-10 12:30:34 +0800

    build: 配置 Vite 基礎路徑並忽略 .env 檔案

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JOCHU_Sohp_Floor_Data_Collection/',
   plugins: [react(), tailwindcss()],
 })
